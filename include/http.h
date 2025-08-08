d307cf1cbab346f3c7720afe4849be267fbd8646
Alexandra Shornikova <smiley-alyx@yandex.ru>
2025-08-08 19:50:09 +0700

    restore proper header/prototypes and implement routing/response helpers

--- include/http.h
+++ include/http.h
@@ -5,6 +5,7 @@
 
 #define MAX_HEADERS 32
 #define MAX_QUERY_PARAMS 32
+#define MAX_BODY 8192
 
 typedef struct { char key[64]; char value[256]; } Header;
 typedef struct { char key[64]; char value[256]; } QueryParam;
@@ -16,21 +17,30 @@ typedef struct {
     int header_count;
     QueryParam query[MAX_QUERY_PARAMS];
     int query_count;
+
+    char content_type[128];
+    char body[MAX_BODY];
+    size_t body_len;
 } HttpRequest;
 
 typedef struct { int client_fd; } HttpResponse;
 
 typedef void (*RouteHandler)(HttpRequest*, HttpResponse*);
 
-void http_parse_request(HttpRequest* req, const char* raw);
+// Parsing & helpers
+void http_parse_request(HttpRequest* req, const char* raw, size_t raw_len);
+const char* http_get_header(HttpRequest* req, const char* key);
+const char* http_get_query(HttpRequest* req, const char* key);
+const char* http_get_form(HttpRequest* req, const char* key);
+
+// Response API
 void http_init_response(HttpResponse* res, int client_fd);
 void http_send_text(HttpResponse* res, const char* body);
-void http_add_route(const char* method, const char* path, RouteHandler handler);
 void http_send_html(HttpResponse* res, const char* html);
 void http_send_bytes(HttpResponse* res, const char* content_type, const unsigned char* data, size_t len);
 
+// Routing
+void http_add_route(const char* method, const char* path, RouteHandler handler);
 void http_handle_route(HttpRequest* req, HttpResponse* res);
-const char* http_get_header(HttpRequest* req, const char* key);
-const char* http_get_query(HttpRequest* req, const char* key);
 
 #endif
